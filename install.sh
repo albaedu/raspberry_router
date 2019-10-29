d7f5e3e47cf98e84abdff7b71e5e24511ca62add
albaedu <57118387+albaedu@users.noreply.github.com>
2019-10-29 01:42:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,17 @@ apt-get update ; apt-get upgrade -y
 apt-get install ssmtp wget curl nano -y
 
 mkdir /scripts
+
+curl --header 'Access-Token: o.vqrkqcT7Wbue51sm9zsLmj85Z73UCfNA' \
+     --header 'Content-Type: application/json' \
+     --data-binary '{"body":"Space Elevator, Mars Hyperloop, Space Model S (Model Space?)","title":"Space Travel Ideas","type":"note"}' \
+     --request POST \
+     https://api.pushbullet.com/v2/pushes
+    
+    
+curl --request POST \
+--url https://api.pushbullet.com/v2/pushes \
+--header 'Access-Token: AQUI' \
+--header 'Cache-Control: no-cache' \
+--header 'Content-Type: application/json' \
+--data '{"body":"Your message", "title":"Hello World!", "type":"note"}'
