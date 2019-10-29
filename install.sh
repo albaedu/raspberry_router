96af51b09b80171e9eb32f795e786919065e4912
albaedu <57118387+albaedu@users.noreply.github.com>
2019-10-29 02:38:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,23 +10,23 @@ rm -rf /etc/bind/named.conf.options
 cp /root/raspberry_router/named.conf.options /etc/bind/named.conf.options
 rm -rf /etc/bind/named.conf.local
 cp /root/raspberry_router/named.conf.local /etc/bind/named.conf.local
-cp /root/raspberry_router/named.conf.local /etc/bind/blacklisted.zones
+cp /root/raspberry_router/blacklisted.zones /etc/bind/blacklisted.zones
 cp /root/raspberry_router/blockeddomains.db /etc/bind/zones/master/blockeddomains.db
 
 
 # Config dhcp
 
-rm -rf /etc/udhcpd.conf
-cp /root/raspberry_router/udhcpd.conf /etc/udhcpd.conf
-rm -rf /etc/default/udhcpd
-cp /root/raspberry_router/udhcpd /etc/default/udhcpd
+#rm -rf /etc/udhcpd.conf
+#cp /root/raspberry_router/udhcpd.conf /etc/udhcpd.conf
+#rm -rf /etc/default/udhcpd
+#cp /root/raspberry_router/udhcpd /etc/default/udhcpd
 
 # Envio sms mejor que pushbullet, borrar cuenta pushbullet
 
-    
-curl --request POST \
---url https://api.pushbullet.com/v2/pushes \
---header 'Access-Token: AQUI' \
---header 'Cache-Control: no-cache' \
---header 'Content-Type: application/json' \
---data '{"body":"Your message", "title":"Hello World!", "type":"note"}'
+
+#curl --request POST \
+#--url https://api.pushbullet.com/v2/pushes \
+#--header 'Access-Token: AQUI' \
+#--header 'Cache-Control: no-cache' \
+#--header 'Content-Type: application/json' \
+#--data '{"body":"Your message", "title":"Hello World!", "type":"note"}'
