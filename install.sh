f7e090663fa3b0a3fa15c6769b7ea364b222f6c8
albaedu <57118387+albaedu@users.noreply.github.com>
2019-10-30 16:00:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,28 @@
-apt-get update ; apt-get upgrade -y
+# Basic software
 
-apt-get install wget curl nano udhcpd bind9 apache2 -y
+apt-get install wget nano bc git curl openssl pwgen zip unzip -y
 
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+# SSH Config
 
-mkdir /scripts
+rootpass=$(pwgen 16 1)
 
-# Config bind
+sudo sed -i "s/#PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
+sudo sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
+sudo sed -i "s/#PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+sudo sed -i "s/PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+echo root:$rootpass | chpasswd
+sudo service ssh restart
+sudo service sshd restart
+echo $rootpass
 
-rm -rf /etc/bind/named.conf.options
-cp /root/raspberry_router/named.conf.options /etc/bind/named.conf.options
-rm -rf /etc/bind/named.conf.local
-cp /root/raspberry_router/named.conf.local /etc/bind/named.conf.local
-mkdir -p /etc/bind/zones/master/
-cp /root/raspberry_router/blockeddomains.db /etc/bind/zones/master/blockeddomains.db
-touch /etc/bind/blacklisted_custom.zones
-wget -O - https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt | sed -e '1,15d' | awk -F[/:] '{print $2}' | awk '{print "zone \""$1"\" in { type master; file \"/etc/bind/zones/blockeddomains.db\"; };"}' > /etc/bind/blacklisted_longlist.zones
+# Wifi Installation
 
-# Cron para el wget
-
-# Config apache ip 10.0.0.2 para ban forzando ip y mostrando pagina custom
-
-# Config dhcp
-
-rm -rf /etc/udhcpd.conf
-cp /root/raspberry_router/udhcpd.conf /etc/udhcpd.conf
-rm -rf /etc/default/udhcpd
-cp /root/raspberry_router/udhcpd /etc/default/udhcpd
-
-# Envio sms mejor que pushbullet, borrar cuenta pushbullet
-
-
-#curl --request POST \
-#--url https://api.pushbullet.com/v2/pushes \
-#--header 'Access-Token: AQUI' \
-#--header 'Cache-Control: no-cache' \
-#--header 'Content-Type: application/json' \
-#--data '{"body":"Your message", "title":"Hello World!", "type":"note"}'
+wget http://www.fars-robotics.net/install-wifi -O /usr/bin/install-wifi
+chmod +x /usr/bin/install-wifi
+sudo install-wifi
 
 rm -rf /root/raspberry_router
 echo ""
+echo "your root password is"
+echo $rootpass
 echo "you should restart"
