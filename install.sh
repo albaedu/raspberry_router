1cbec91ee8d95a19c9e3eb68702e7496fb799975
albaedu <57118387+albaedu@users.noreply.github.com>
2019-10-31 01:31:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,8 +32,13 @@ rm -rf /etc/dhcpcd.conf
 cp /root/raspberry_router/dhcpcd.conf /etc/dhcpcd.conf
 
 # Internet share config
+
 rm -rf /etc/sysctl.conf
 cp /root/raspberry_router/sysctl.conf /etc/sysctl.conf
+iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
+sh -c "iptables-save > /etc/iptables.ipv4.nat"
+rm -rf /etc/rc.local
+cp /root/raspberry_router/rc.local /etc/rc.local
 
 # DHCP Server config
 
